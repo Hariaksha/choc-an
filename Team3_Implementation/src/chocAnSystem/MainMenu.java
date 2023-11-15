861fe225dda783043151cb4dbcde5ef77b16791c
Hariaksha <hgunda@crimson.ua.edu>
2023-11-15 14:27:12 -0600

    Merge branch 'master' of https://Hariaksha@bitbucket.org/tklocklear/fall2023team3.git

--- Team3_Implementation/src/chocAnSystem/MainMenu.java
+++ Team3_Implementation/src/chocAnSystem/MainMenu.java
@@ -21,12 +21,16 @@ public class MainMenu {
       System.out.println("You entered " + loginNumber + " as your login ID number.");
       if (loginNumber >= 0 && loginNumber < 100) {
         System.out.println("You are a Provider. You will be directed to the Provider Terminal");
+        ProviderTerminal pt = new ProviderTerminal();
+        pt.chooseAction();
       } 
       else if (loginNumber >= 100 && loginNumber < 200) {
         System.out.println("You are an Operator. You will be directed to the Operator Terminal");
       } 
       else if (loginNumber >= 200 && loginNumber < 300) {
         System.out.println("You are a Manager. You will be directed to the Manager Terminal");
+        ManagerTerminal mt = new ManagerTerminal();
+        mt.chooseAction();
       } 
       else {
         System.out.println("You have entered an invalid login. Please try again.");
