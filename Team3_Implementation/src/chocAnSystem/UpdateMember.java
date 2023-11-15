82cafd7a40d554ffa403caac29da39048a9a3b89
alhoward3 <alhoward3@crimson.ua.edu>
2023-11-15 13:47:26 -0600

    Update Member and Provider

---
+++ Team3_Implementation/src/chocAnSystem/UpdateMember.java
@@ -0,0 +1,102 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner; 
+import java.util.Random; 
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+class Member
+{  
+    String name; 
+    int number; 
+    String street; 
+    String city; 
+    String zip; 
+    
+}
+//////////////////////////////////////////////////////////////////////////
+class UpdateMember
+{ 
+    public void addMember() 
+    { 
+        Member newMember = new Member(); 
+
+        Scanner myObj = new Scanner(System.in); 
+
+        System.out.print("Enter Name: "); 
+
+        newMember.name = myObj.nextLine(); 
+        
+        System.out.print("Enter Street: "); 
+
+        newMember.street = myObj.nextLine(); 
+
+        System.out.print("Enter City: "); 
+
+        newMember.city = myObj.nextLine(); 
+
+        System.out.print("Enter Zip: "); 
+
+        newMember.zip = myObj.nextLine(); 
+
+        Random random = new Random(); 
+        int n = 100000000 + random.nextInt(900000000);
+        newMember.number = n; 
+
+        updateMemberList(newMember);
+    } 
+/////////////////////////////////////////////////////////////////////////////////  
+    public void deleteMember() 
+    { 
+        System.out.println("Enter Member ID Number: ");
+        Scanner input = new Scanner(System.in);  
+        String id = input; 
+
+        try {
+            Scanner scan = new Scanner(new File("MemberList.txt"));
+            PrintWriter writer = new PrintWriter(new FileWriter("tempList.txt")))
+            scan.nextLine();
+
+            while(scan.hasNextLine())
+            { 
+
+            }
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+        
+    }
+//////////////////////////////////////////////////////////////////////////////////////
+    public void updateMemberRecords() 
+    { 
+        System.out.println("Enter Member ID Number: ");
+    } 
+//////////////////////////////////////////////////////////////////////////////////////
+    public void updateMemberList(Member member) 
+    {
+        try{
+            FileWriter reader = new FileWriter("MemberList.txt", true);
+            BufferedWriter writer = new BufferedWriter(reader);
+            writer.write(member.name + " / " + member.number + " / " + member.street + " / " + member.city + " / " + member.zip);
+            writer.newLine();
+            writer.close();
+            reader.close();
+        }
+        catch(IOException e){
+            e.printStackTrace(); 
+        }
+    }
+////////////////////////////////////////////////////////////////////////////////////////
+    public static void main(String args[])
+    {
+        Member member = new Member();
+        UpdateMember update = new UpdateMember();
+        update.addMember(); 
+        update.addMember(); 
+    }
+
+} 
+
---
+++ Team3_Implementation/src/chocAnSystem/UpdateProvider.java
@@ -0,0 +1,34 @@
+
+public class Provider
+{ 
+    String name; 
+    int number; 
+    Street street; 
+    String city; 
+    int zip; 
+
+}
+
+
+class UpdateProvider
+{ 
+    public static void main(String args[])
+    { 
+       UpdateProvider provider = new UpdateProvider(); 
+       provider.updateProviderRecords(); 
+    }
+    public void addProvider() 
+    { 
+         System.out.println("Enter New Member Info: "); 
+    } 
+      
+    public void deleteProvider() 
+    { 
+        System.out.println("Enter New Member Info: ");
+    }
+
+    public void updateProviderRecords() 
+    { 
+        System.out.println("Enter Member ID Number: ");
+    } 
+} 
