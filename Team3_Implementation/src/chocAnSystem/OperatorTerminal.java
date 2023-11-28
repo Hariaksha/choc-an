31e7563066a6312ceeadf5bc2ac79af0e46d4b0c
Ethan Busby <esbusby@crimson.ua.edu>
2023-11-28 11:30:18 -0600

    Merge branch 'master' of https://esbusby@bitbucket.org/tklocklear/fall2023team3.git

--- Team3_Implementation/src/chocAnSystem/OperatorTerminal.java
+++ Team3_Implementation/src/chocAnSystem/OperatorTerminal.java
@@ -1,5 +1,22 @@
 package chocAnSystem;
+import java.util.Scanner;
 
 public class OperatorTerminal {
+	public String operatorPassword;
+	public String operatorUsername;
+	
+	public OperatorTerminal() {
+	    operatorUsername = "random username";
+	    operatorPassword = "Password";
+	  }
+	
+	public OperatorTerminal(String pass, String name) {
+	    operatorUsername = name;
+	    operatorPassword = pass;
+	    }
+	
+	public void chooseAction() {
+		Scanner sc = new Scanner(System.in);
 
+	}
 }
