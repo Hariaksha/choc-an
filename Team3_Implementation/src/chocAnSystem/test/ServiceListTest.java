089f51913772a65a107b99258b26c990630bf747
tbcruise <tbcruise@crimson.ua.edu>
2023-11-28 12:09:00 -0600

    Added Junit for ServiceList

---
+++ Team3_Implementation/src/chocAnSystem/test/ServiceListTest.java
@@ -0,0 +1,19 @@
+package chocAnSystem.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ServiceListTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
