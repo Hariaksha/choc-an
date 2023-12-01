7b1135175d78fe73b93413b21c0956bfc7aa88f2
alhoward3 <alhoward3@crimson.ua.edu>
2023-12-01 12:47:04 -0600

    Updated "Update Member" and "Update Provider"

--- Team3_Implementation/src/chocAnSystem/UpdateMember.java
+++ Team3_Implementation/src/chocAnSystem/UpdateMember.java
@@ -1,11 +1,15 @@
-package chocAnSystem; 
+package chocAnSystem;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.ArrayList;
 import java.util.Random; 
 import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 class Member
@@ -16,77 +20,258 @@ class Member
     String city; 
     String state; 
     String zip; 
-    
+
+    public Member(String name, int number, String street, String city, String state, String zip) 
+    {
+        this.name = name;
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    } 
+    public Member(){}
 }
+
 //////////////////////////////////////////////////////////////////////////
 class UpdateMember
 { 
+    ArrayList <Member> memberList = new ArrayList<>();
+    Scanner scan = new Scanner(System.in); 
+
+    public void readMemberList()
+    { 
+        try{
+            File  file = new File("MemberList.txt"); 
+            BufferedReader reader = new BufferedReader(new FileReader(file)); 
+            String line;
+            while ((line = reader.readLine()) != null)
+            { 
+                String [] parts = line.split(" / " ); 
+                if(parts.length == 6) 
+                { 
+                    String name = parts[0]; 
+                    int number = Integer.parseInt(parts[1]); 
+                    String street = parts[2]; 
+                    String city = parts[3]; 
+                    String state = parts[4]; 
+                    String zip = parts[5]; 
+
+                    Member member = new Member(name, number, street, city, state, zip); 
+                    memberList.add(member); 
+                }
+                else{ 
+                    System.out.println("Invailid line format: " + line); 
+                }
+            } 
+        }
+        catch (IOException e) 
+        { 
+            e.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void addMember() 
     { 
         Member newMember = new Member(); 
 
-        Scanner myObj = new Scanner(System.in); 
-
         System.out.print("Enter Name: "); 
 
-        newMember.name = myObj.nextLine(); 
+        newMember.name = scan.nextLine(); 
         
         System.out.print("Enter Street: "); 
 
-        newMember.street = myObj.nextLine(); 
+        newMember.street = scan.nextLine(); 
 
         System.out.print("Enter City: "); 
 
-        newMember.city = myObj.nextLine(); 
+        newMember.city = scan.nextLine(); 
 
         System.out.print("Enter State: "); 
 
-        newMember.state = myObj.nextLine();
+        newMember.state = scan.nextLine();
 
         System.out.print("Enter Zip: "); 
 
-        newMember.zip = myObj.nextLine(); 
+        newMember.zip = scan.nextLine(); 
 
         Random random = new Random(); 
         int n = 100000000 + random.nextInt(900000000);
         newMember.number = n; 
 
-        updateMemberList(newMember);
+        memberList.add(newMember); 
+        updateMemberList(); 
+
+        System.out.println("You you like to continue to add another member? YES or NO"); 
+        boolean cont  = false; 
+        String input; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                addMember();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return;  
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update member list? YES or NO"); 
+            }
+        } 
     } 
-/////////////////////////////////////////////////////////////////////////////////  
+
+    /////////////////////////////////////////////////////////////////////////////////  
     public void deleteMember() 
     { 
-        /* 
         System.out.println("Enter Member ID Number: ");
-        Scanner input = new Scanner(System.in);  
-        String id = input; 
+        int idNum = Integer.parseInt(scan.nextLine()); 
+        boolean found = false; 
 
-        try {
-            Scanner scan = new Scanner(new File("MemberList.txt"));
-            PrintWriter writer = new PrintWriter(new FileWriter("tempList.txt"));
-            scan.nextLine();
-
-            while(scan.hasNextLine())
+        for(int i = 0; i < memberList.size(); i++)
+        { 
+            if(memberList.get(i).number == idNum)
             { 
-
+                found = true; 
+                memberList.remove(i);
+                System.out.println("Member with ID " + idNum + " deleted successfully.");
             }
-        } catch (IOException e) {
-           e.printStackTrace();
         }
-        */
+
+        if(!found)
+        { 
+            System.out.println("Member not found");
+            deleteMember();
+            return; 
+        }
+
+        updateMemberList();
+
+        System.out.println("You you like to delete other members? YES or NO"); 
+        boolean cont  = false; 
+        String input; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                deleteMember();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return; 
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update member list? YES or NO"); 
+            }
+        } 
     }
-//////////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////////
     public void updateMemberRecords() 
     { 
-        System.out.println("Enter Member ID Number: ");
+        System.out.println("Enter Member ID Number: "); 
+        int idNum = Integer.parseInt(scan.nextLine()); 
+        boolean vaild = false; 
+        int memberLocation = 0; 
+        boolean found = false; 
+
+        for(int i = 0; i < memberList.size(); i++)
+        { 
+            if(memberList.get(i).number == idNum)
+            { 
+                found = true; 
+                memberLocation = i; 
+                break; 
+            }
+        } 
+
+        if(!found)
+        { 
+            System.out.println("Member not found");
+            deleteMember();
+            return; 
+        }
+
+        System.out.println("To update member name type NAME"); 
+        System.out.println("To update member address type ADDRESS"); 
+        while(!vaild)
+        { 
+            String input = scan.nextLine(); 
+
+            if(input.equalsIgnoreCase("NAME"))
+            { 
+                vaild = true;
+                System.out.print("Enter Name: "); 
+                memberList.get(memberLocation).name = scan.nextLine();
+            }
+            else if(input.equalsIgnoreCase("ADDRESS"))
+            { 
+                vaild = true; 
+                System.out.print("Enter Street: ");
+                memberList.get(memberLocation).street = scan.nextLine();
+                
+                System.out.print("Enter City: "); 
+                memberList.get(memberLocation).city = scan.nextLine();
+            
+                System.out.print("Enter State: "); 
+                memberList.get(memberLocation).state = scan.nextLine();
+            
+                System.out.print("Enter Zip: "); 
+                memberList.get(memberLocation).zip = scan.nextLine();
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("To update member name type NAME"); 
+                System.out.println("To update member address type ADDRESS"); 
+            }
+        }
+
+        updateMemberList();
+
+        System.out.println("You you like to continue to update other member records? YES or NO"); 
+        boolean cont  = false; 
+        String input;
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                updateMemberRecords();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return; 
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update member list? YES or NO"); 
+            }
+        } 
     } 
-//////////////////////////////////////////////////////////////////////////////////////
-    public void updateMemberList(Member member) 
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    public void updateMemberList() 
     {
         try{
-            FileWriter reader = new FileWriter("MemberList.txt", true);
+            FileWriter reader = new FileWriter("MemberList.txt");
             BufferedWriter writer = new BufferedWriter(reader);
-            writer.write(member.name + " / " + member.number + " / " + member.street + " / " + member.city + " / " + member.state + " / " + member.zip);
+            for(int i = 0; i < memberList.size(); i++)
+            {
+                writer.write(memberList.get(i).name + " / " + memberList.get(i).number + " / " + memberList.get(i).street + " / " + memberList.get(i).city + " / " + memberList.get(i).state + " / " + memberList.get(i).zip + "\n");
+            }
+
             writer.newLine();
             writer.close();
             reader.close();
@@ -95,39 +280,74 @@ class UpdateMember
             e.printStackTrace(); 
         }
     }
-////////////////////////////////////////////////////////////////////////////////////////
 
-    public void start()
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public void startMemberUpdate()
     { 
-        System.out.println("To add member type add"); 
-        System.out.println("To delete member type delete"); 
-        System.out.println("To update member records type update"); 
+        readMemberList();
+        System.out.println("To add member type ADD"); 
+        System.out.println("To delete member type DELETE"); 
+        System.out.println("To update member records type UPDATE"); 
         Boolean vaild = false;
-        Scanner scan  = new Scanner(System.in); 
+        String input; 
+
         while(!vaild)
         {
-            String input =  scan.nextLine(); 
-            if(input.equals("add"))
+            input =  scan.nextLine(); 
+            if(input.equalsIgnoreCase("ADD"))
             { 
                 vaild = true;
                 addMember(); 
             }
-            else if(input.equals("delete"))
+            else if(input.equalsIgnoreCase("DELETE"))
             { 
                 vaild = true; 
                 deleteMember();
             }
-            else if(input.equals("update"))
+            else if(input.equalsIgnoreCase("UPDATE"))
             { 
                 vaild = true; 
-                Member newMember = new Member(); 
-                updateMemberList(newMember);
+                updateMemberRecords(); 
             }
             else 
             { 
                 System.out.println("Enter valid function");
+                System.out.println("To add member type ADD"); 
+                System.out.println("To delete member type DELETE"); 
+                System.out.println("To update member records type UPDATE"); 
             }
         } 
+
+        System.out.println("You you like to continue to update member list? YES or NO"); 
+        boolean cont  = false; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                startMemberUpdate();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return;  
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update member list? YES or NO"); 
+            }
+        } 
+        
+        updateMemberList();
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public static void main(String args[])
+    {
+        UpdateMember update = new UpdateMember();  
+        update.startMemberUpdate(); 
+    }
+
 } 
 
--- Team3_Implementation/src/chocAnSystem/UpdateProvider.java
+++ Team3_Implementation/src/chocAnSystem/UpdateProvider.java
@@ -1,34 +1,353 @@
-package chocAnSystem; 
-public class Provider
-{ 
+package chocAnSystem;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.ArrayList;
+import java.util.Random; 
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+class Provider
+{  
     String name; 
     int number; 
-    Street street; 
+    String street; 
     String city; 
-    int zip; 
+    String state; 
+    String zip; 
 
+    public Provider(String name, int number, String street, String city, String state, String zip) 
+    {
+        this.name = name;
+        this.number = number;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    } 
+    public Provider(){}
 }
 
-
+//////////////////////////////////////////////////////////////////////////
 class UpdateProvider
 { 
-    public static void main(String args[])
+    ArrayList <Provider> providerList = new ArrayList<>();
+    Scanner scan = new Scanner(System.in); 
+
+    public void readProviderList()
     { 
-       UpdateProvider provider = new UpdateProvider(); 
-       provider.updateProviderRecords(); 
-    }
-    public void addProvider() 
-    { 
-         System.out.println("Enter New Member Info: "); 
-    } 
-      
-    public void deleteProvider() 
-    { 
-        System.out.println("Enter New Member Info: ");
+        try{
+            File  file = new File("ProviderList.txt"); 
+            BufferedReader reader = new BufferedReader(new FileReader(file)); 
+            String line;
+            while ((line = reader.readLine()) != null)
+            { 
+                String [] parts = line.split(" / " ); 
+                if(parts.length == 6) 
+                { 
+                    String name = parts[0]; 
+                    int number = Integer.parseInt(parts[1]); 
+                    String street = parts[2]; 
+                    String city = parts[3]; 
+                    String state = parts[4]; 
+                    String zip = parts[5]; 
+
+                    Provider provider = new Provider(name, number, street, city, state, zip); 
+                    providerList.add(provider); 
+                }
+                else{ 
+                    System.out.println("Invailid line format: " + line); 
+                }
+            } 
+        }
+        catch (IOException e) 
+        { 
+            e.printStackTrace();
+        }
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void addProvider() 
+    { 
+        Provider newProvider = new Provider(); 
+
+        System.out.print("Enter Name: "); 
+
+        newProvider.name = scan.nextLine(); 
+        
+        System.out.print("Enter Street: "); 
+
+        newProvider.street = scan.nextLine(); 
+
+        System.out.print("Enter City: "); 
+
+        newProvider.city = scan.nextLine(); 
+
+        System.out.print("Enter State: "); 
+
+        newProvider.state = scan.nextLine();
+
+        System.out.print("Enter Zip: "); 
+
+        newProvider.zip = scan.nextLine(); 
+
+        Random random = new Random(); 
+        int n = 100000000 + random.nextInt(900000000);
+        newProvider.number = n; 
+
+        providerList.add(newProvider); 
+        updateProviderList(); 
+
+        System.out.println("You you like to continue to add another provider? YES or NO"); 
+        boolean cont  = false; 
+        String input; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                addProvider();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return;  
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update provider list? YES or NO"); 
+            }
+        } 
+    } 
+
+    /////////////////////////////////////////////////////////////////////////////////  
+    public void deleteProvider() 
+    { 
+        System.out.println("Enter Provider ID Number: ");
+        int idNum = Integer.parseInt(scan.nextLine()); 
+        boolean found = false; 
+
+        for(int i = 0; i < providerList.size(); i++)
+        { 
+            if(providerList.get(i).number == idNum)
+            { 
+                found = true; 
+                providerList.remove(i);
+                System.out.println("Provider with ID " + idNum + " deleted successfully.");
+            }
+        }
+
+        if(!found)
+        { 
+            System.out.println("Provider not found");
+            deleteProvider();
+            return; 
+        }
+
+        updateProviderList();
+
+        System.out.println("You you like to delete other providers? YES or NO"); 
+        boolean cont  = false; 
+        String input; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                deleteProvider();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return; 
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update provider list? YES or NO"); 
+            }
+        } 
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
     public void updateProviderRecords() 
     { 
-        System.out.println("Enter Member ID Number: ");
+        System.out.println("Enter Provider ID Number: "); 
+        int idNum = Integer.parseInt(scan.nextLine()); 
+        boolean vaild = false; 
+        int providerLocation = 0; 
+        boolean found = false; 
+
+        for(int i = 0; i < providerList.size(); i++)
+        { 
+            if(providerList.get(i).number == idNum)
+            { 
+                found = true; 
+                providerLocation = i; 
+                break; 
+            }
+        } 
+
+        if(!found)
+        { 
+            System.out.println("Provider not found");
+            deleteProvider();
+            return; 
+        }
+
+        System.out.println("To update Provider name type NAME"); 
+        System.out.println("To update Provider address type ADDRESS"); 
+        while(!vaild)
+        { 
+            String input = scan.nextLine(); 
+
+            if(input.equalsIgnoreCase("NAME"))
+            { 
+                vaild = true;
+                System.out.print("Enter Name: "); 
+                providerList.get(providerLocation).name = scan.nextLine();
+            }
+            else if(input.equalsIgnoreCase("ADDRESS"))
+            { 
+                vaild = true; 
+                System.out.print("Enter Street: ");
+                providerList.get(providerLocation).street = scan.nextLine();
+                
+                System.out.print("Enter City: "); 
+                providerList.get(providerLocation).city = scan.nextLine();
+            
+                System.out.print("Enter State: "); 
+                providerList.get(providerLocation).state = scan.nextLine();
+            
+                System.out.print("Enter Zip: "); 
+                providerList.get(providerLocation).zip = scan.nextLine();
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("To update Provider name type NAME"); 
+                System.out.println("To update Provider address type ADDRESS"); 
+            }
+        }
+
+        updateProviderList();
+
+        System.out.println("You you like to continue to update other provider records? YES or NO"); 
+        boolean cont  = false; 
+        String input;
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                updateProviderRecords();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return; 
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update Proivder list? YES or NO"); 
+            }
+        } 
     } 
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    public void updateProviderList() 
+    {
+        try{
+            FileWriter reader = new FileWriter("ProviderList.txt");
+            BufferedWriter writer = new BufferedWriter(reader);
+            for(int i = 0; i < providerList.size(); i++)
+            {
+                writer.write(providerList.get(i).name + " / " + providerList.get(i).number + " / " + memberList.get(i).street + " / " + memberList.get(i).city + " / " + memberList.get(i).state + " / " + memberList.get(i).zip + "\n");
+            }
+
+            writer.newLine();
+            writer.close();
+            reader.close();
+        }
+        catch(IOException e){
+            e.printStackTrace(); 
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public void startProviderUpdate()
+    { 
+        readProviderList();
+        System.out.println("To add Provider type ADD"); 
+        System.out.println("To delete Provider type DELETE"); 
+        System.out.println("To update Provider records type UPDATE"); 
+        Boolean vaild = false;
+        String input; 
+
+        while(!vaild)
+        {
+            input =  scan.nextLine(); 
+            if(input.equalsIgnoreCase("ADD"))
+            { 
+                vaild = true;
+                addProvider();
+            }
+            else if(input.equalsIgnoreCase("DELETE"))
+            { 
+                vaild = true; 
+                deleteProvider();
+            }
+            else if(input.equalsIgnoreCase("UPDATE"))
+            { 
+                vaild = true; 
+                updateProviderRecords(); 
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("To add Provider type ADD"); 
+                System.out.println("To delete Provider type DELETE"); 
+                System.out.println("To update Provider records type UPDATE"); 
+            }
+        } 
+
+        System.out.println("You you like to continue to update provider list? YES or NO"); 
+        boolean cont  = false; 
+        while(!cont)
+        { 
+            input = scan.nextLine(); 
+            if(input.equalsIgnoreCase("YES"))
+            { 
+                startProviderUpdate();
+                return; 
+            } 
+            else if(input.equalsIgnoreCase("NO"))
+            { 
+                return;  
+            }
+            else 
+            { 
+                System.out.println("Enter valid function");
+                System.out.println("You you like to continue to update Provider list? YES or NO"); 
+            }
+        } 
+        
+        updateProviderList();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public static void main(String args[])
+    {
+        UpdatePrvider update = new UpdateProvider(); 
+        update.startProviderUpdate(); 
+    }
+
 } 
+
