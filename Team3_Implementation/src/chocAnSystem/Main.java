2913ef64852141e46a48065b88f36691ea597ae9
tbcruise <tbcruise@crimson.ua.edu>
2023-12-03 14:52:41 -0600

    WEEEEEEEEEEEEEEEEEEEEEEE

--- Team3_Implementation/src/chocAnSystem/Main.java
+++ Team3_Implementation/src/chocAnSystem/Main.java
@@ -1,10 +1,12 @@
 /** @author all team members: Ethan Busby, Adam Howard, Hariaksha Gunda, Ryan McCulley, Robert Read, Tyler Cruise, and James White*/
 package chocAnSystem;
 
+import java.io.IOException;
+
 /** This Main class is where the Main Menu class and its functions are used.*/
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainMenu menu = new MainMenu();
         menu.display();
         menu.login();
