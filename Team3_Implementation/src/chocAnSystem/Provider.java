57f9ce9768b677d2d8729cdf92b7940bd52a80c6
remcculley <remcculley@crimson.ua.edu>
2023-12-03 13:24:43 -0600

    Pushing provider changes

--- Team3_Implementation/src/chocAnSystem/Provider.java
+++ Team3_Implementation/src/chocAnSystem/Provider.java
@@ -1,62 +1,18 @@
+//Author Ryan McCulley
 package chocAnSystem;
 
 public class Provider {
-
-	public int providerNumber;
-    public String providerName;
-    public String providerAddress;
-    public String providerCity;
-    public String providerState;
-    int providerZip;
-
-    //Gets the Provider number
-    public int getProviderNumber(Provider pClass){
-        int meow = pClass.providerNumber;
-        return meow;
-    }
-    //Gets the Provider Name
-    public String getProviderName(Provider pClass){
-    	String meow = pClass.providerName;
-        return meow;
-    }
-    //Gets the Provider Address
-    public String getProviderAddress(Provider pClass){
-    	String meow = pClass.providerAddress;
-        return meow;
-    }
-    //Gets the Provider City
-    public String getProviderCity(Provider pClass){
-    	String meow = pClass.providerCity;
-        return meow;
-    }
-    //Gets the Provider State
-    public String getProviderState(Provider pClass){
-    	String meow = pClass.providerState;
-        return meow;
-    }
-    //Gets the Provider Zip 
-    public int getProviderZip(Provider pClass){
-    	int meow = pClass.providerZip;
-        return meow;
-    }
-    
-    //creates a new provider with all the inputs
-    public Provider (int pNum, String pName, String pAdd, String pCity, String pState, int pZip) {
-    	Provider meow = new Provider();
-    	meow.providerNumber = pNum;
-    	meow.providerName = pName;
-    	meow.providerAddress = pAdd;
-    	meow.providerCity = pCity;
-    	meow.providerState = pState;
-    	meow.providerZip = pZip;
-    }
     
     //basic Provider constructor
     public Provider() {
     
     }
-     
+    //This lets the Provider access the provider terminal and call its actions
+    public void Paction() {
+    	System.out.println("You are a Provider. You will be directed to the Manager Terminal.");
+    	ProviderTerminal pt = new ProviderTerminal();
+    	pt.providerLogin();
+    	pt.chooseAction();
+    }
 
 }
-
-//fix
