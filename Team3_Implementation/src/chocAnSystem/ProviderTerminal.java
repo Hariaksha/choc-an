ab32e14fb0036fe5dc39fea8b5e41e392dc9dc10
remcculley <remcculley@crimson.ua.edu>
2023-12-03 13:23:37 -0600

    Pushing provider terminal updates

--- Team3_Implementation/src/chocAnSystem/ProviderTerminal.java
+++ Team3_Implementation/src/chocAnSystem/ProviderTerminal.java
@@ -1,73 +1,108 @@
+// Author: Ryan McCulley
 package chocAnSystem;
-
 import java.util.Scanner;
-import java.util.ArrayList;
 
+/** This declares the class attributes. */
+public class ProviderTerminal {
+  private String providerUsername;
+  private String providerPassword;
+  private String passwordArr[] = {"121925030", "password649"}; // This variable and the next one are parallel arrays storing matching username and password pairs.
+  private String usernameArr[] = {"Ryan", "Busby"};
+  public Scanner sc = new Scanner(System.in);
 
-public class ProviderTerminal{
+  // This is a Default Constructor
+  public ProviderTerminal() {
+	providerUsername = "Meowy";
+	providerPassword = "meowPass";
+  }
 
-    public String providerUsername;
-    public String providerPassword;
-
-    public String action;
-
-    public ArrayList<String> Usernames = new ArrayList<String>();
-    public ArrayList<String> Passwords = new ArrayList<String>();
-    
-    public void createToPList() {
-    	Usernames.add("Ryan");
-        Usernames.add("Hari");
-        Usernames.add("Ethan");
-        Passwords.add("121925030");
-        Passwords.add("122381670");
-        Passwords.add("122048720");
+  // This is the Primary Constructor with parameters
+  public ProviderTerminal(String pass, String name) {
+	providerUsername = name;
+    providerPassword = pass;
     }
 
-     public String chooseAction(){
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Choose Action: Bill ChocAn, Verify Member, Request Directory");
-        action = obj.nextLine();
-        obj.close();
-        if (action != "Bill ChocAn" || action != "Verify Member" || action != "Request Directory") return chooseAction();
-        return "Nothing Selected";
-    }
-
-    public boolean providerLogin(){
-        Scanner obj = new Scanner(System.in);
-
-        int count = 0;
-
-        System.out.println("Enter username");
-        providerUsername = obj.nextLine();
-
-        System.out.println("Enter password");
-        providerPassword = obj.nextLine();
-
-        String meowUsername = providerUsername;
-        String meowPassword = providerPassword;
-
-        for(int i = 0; i < Usernames.size(); i++){
-            if (meowUsername == Usernames.get(i)){
-                count = i;
-                break;
-            }
-        }
-        obj.close();
-        if(meowPassword == Passwords.get(count)){
-            return true;
-        }
-        return false;
-    }
-
-    public String verifyProvider(boolean meow){
-        if(meow == true){
-        	return "Logging in";
-        }
-        return "Invalid Log In";
-        
-    }
-
-
+  // This method allows the provider to login
+  public void providerLogin() {
+	  // Prompt user for username
+	  System.out.println("Enter username and click Enter: ");
+	  String username = sc.nextLine();
+	  // Prompt user for p	assword
+	  System.out.println("Enter password and click Enter: ");
+	  String password = sc.nextLine();
+	  // Check if username is valid and return its index in username array. Return -1 if not found
+	  int isUsernameValid = isStrInUsernameArr(username);
+	  boolean loggedIn; // Create loggedIn boolean variable to continue prompting user for login info through while loop
+	  while(true) {
+		  if (isUsernameValid == -1) { // If username is wrong, user is not logged in
+			  loggedIn = false;
+		  }
+		  else {
+			  if (!isPasswordRight(password, isUsernameValid)) { // if password is wrong, user is not logged in
+				  loggedIn = false;
+			  }
+			  else { // user is logged in if username and password is correct
+				  loggedIn = true;
+			  }
+		  }
+		  // Tell user if login credentials worked
+		  if (!loggedIn) {
+			  System.out.println("Invalid credentials. Please try again");
+		  }
+		  else if(loggedIn) {
+			  System.out.println("Successful login.");
+			  break;
+		  }
+		  // Reprompt user for login credentials if loop does not break
+		  System.out.println("Enter username and click Enter: ");
+		  username = sc.nextLine();
+		  System.out.println("Enter password and click Enter: ");
+		  password = sc.nextLine();
+		  isUsernameValid = isStrInUsernameArr(username);
+	  }
+  }
+  
+  // This method allows a provider to choose an action. */
+  public void chooseAction() {
+    ProviderController pc = new ProviderController(); // Creates a ProviderController
+	System.out.println("Choose Action: Bill Choc An, Verify Member, Request Directory"); // Prompt user to choose an action between bill chocAn, Verify Member, and Request Directory
+	String response = sc.nextLine();
+	
+	while (!(response.equals("Bill Choc An") || response.equals("Verify Member") || response.equals("Request Directory"))) { // Prompt for correct response until correct response is given
+		System.out.println("You entered: " + response);
+		if (!(response.equals("Bill Choc An") || response.equals("Verify Member") || response.equals("Request Directory"))) {
+			System.out.println("You have entered an invalid input. Please try again"); // Reprompt
+			response = sc.nextLine();
+		}
+	}
+	
+	if (response.equals("Bill Choc An")) {
+		pc.billChocAn(); // Runs Bill Choc An
+	}
+	else if (response.equals("Verify Member")) {
+		pc.verifyMember(); // Runs Verify Member
+	}
+	else if (response.equals("Request Directory")) {
+		pc.requestDirectory(); // Runs Request Directory
+	}
+	return;
+  }
+  
+  //This method checks to see if the username entered is a valid username. It returns -1 if not valid and the index of the username in the username array if it is valid.
+  public int isStrInUsernameArr(String str) {
+	  for (int i = 0; i < usernameArr.length; i++) { // Use for loop to iterate through string
+		  if (str.equals(usernameArr[i])) { // If entered username equals current element in array
+			  return i; // Return index found
+		  }
+	  }
+	  return -1; // Return -1 if username does not exist.
+  }
+  
+  /**This method checks to see if the password is correct. It is only called if the username is valid, and it checks that the password entered is the password found at the index of the correct username in the password array.*/
+  public boolean isPasswordRight(String str, int index) {
+	  if (str.equals(passwordArr[index])) { // Use str.equals() method to see if password matches username
+		  return true; // Return true if password is right
+	  }
+	  return false; // Return false if password is wrong
+  }
 }
-
-//oops
