a71f6a97e55cdbbfad8e6b3c63a8a7a0e9179366
tbcruise <tbcruise@crimson.ua.edu>
2023-12-03 13:10:59 -0600

    New

---
+++ Team3_Implementation/src/chocAnSystem/test/TylerCruiseManagerTerminalFailureTest.java
@@ -0,0 +1,23 @@
+package chocAnSystem.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import chocAnSystem.ManagerTerminal;
+
+class TylerCruiseManagerTerminalFailureTest {
+	ManagerTerminal t;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
