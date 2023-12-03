52393f8b65b709cf3607e05bd21ed03ba129aeb5
tbcruise <tbcruise@crimson.ua.edu>
2023-12-03 14:53:57 -0600

    WEEEEEEEEEEEEE 3.0

--- Team3_Implementation/src/chocAnSystem/Manager.java
+++ Team3_Implementation/src/chocAnSystem/Manager.java
@@ -1,14 +1,17 @@
 /** @author Hari Gunda*/
 package chocAnSystem;
 
+import java.io.IOException;
+
 public class Manager {
 
     /** This constructor creates instances of the Manager class.*/
     public Manager() {
     }
 
-    /** This demonstrates that the Manager can access the terminal and call its functions. This function returns nothing.*/
-    public void action() {
+    /** This demonstrates that the Manager can access the terminal and call its functions. This function returns nothing.
+     * @throws IOException */
+    public void action() throws IOException {
     	System.out.println("You are a Manager. You will be directed to the Manager Terminal.");
     	ManagerTerminal mt = new ManagerTerminal();
     	mt.login();
